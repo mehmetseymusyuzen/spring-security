192b302abffc568cd5b14d28b9d1617bfb8a8966
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-05 11:52:49 +0100

    added PublicController.java --> in-memory

---
+++ in-memory/src/main/java/com/spring/security/inmemory/controller/PrivateController.java
@@ -0,0 +1,28 @@
+package com.spring.security.inmemory.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/private")
+public class PrivateController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World! from private endpoint";
+    }
+
+    // @PreAuthorize("hasRole('USER')") EndPoint seviyesinde rol verme islemi bu sekilde yapilir
+    @GetMapping("/user")
+    public String helloWorldUserPrivate() {
+        return "Hello World! from user private endpoint";
+    }
+
+    // @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String helloWorldAdminPrivate() {
+        return "Hello World! from admin private endpoint";
+    }
+}
