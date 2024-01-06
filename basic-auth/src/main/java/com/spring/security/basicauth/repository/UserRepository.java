4acf1af0f625158acf1daa5b6b363b4836c95ca9
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-06 12:00:28 +0100

    Create UserRepository.java --> basic-auth

---
+++ basic-auth/src/main/java/com/spring/security/basicauth/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.spring.security.basicauth.repository;
+
+import com.spring.security.basicauth.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(final String userName);
+
+}
