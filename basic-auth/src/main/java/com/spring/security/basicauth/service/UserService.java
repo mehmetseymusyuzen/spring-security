ac7d359dba74803a09e8a58d913b26ff5a78ae08
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-07 12:54:09 +0100

    Create UserService.java --> basic-auth

---
+++ basic-auth/src/main/java/com/spring/security/basicauth/service/UserService.java
@@ -0,0 +1,39 @@
+package com.spring.security.basicauth.service;
+
+import com.spring.security.basicauth.config.PasswordEncoderConfig;
+import com.spring.security.basicauth.dto.CreateUserRequest;
+import com.spring.security.basicauth.model.User;
+import com.spring.security.basicauth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+
+    public Optional<User> getByUserName(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User createUser(CreateUserRequest request) {
+        User newUser = User.builder()
+                .name(request.name())
+                .username(request.username())
+                .password(passwordEncoder.encode(request.password()))
+                .authorities(request.authorities())
+                .accountNonExpired(true)
+                .credentialsNonExpired(true)
+                .isEnabled(true)
+                .accountNonLocked(true)
+                .build();
+
+        return userRepository.save(newUser);
+    }
+}
