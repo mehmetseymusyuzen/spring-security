327618e66b8e41fc2e42c99fb9617a1dd5cbd2cd
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-07 12:53:27 +0100

    Create UserDetailsServiceImpl.java --> basic-auth

---
+++ basic-auth/src/main/java/com/spring/security/basicauth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.spring.security.basicauth.service;
+
+import com.spring.security.basicauth.model.User;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> user = userService.getByUserName(username);
+
+        return user.orElseThrow(EntityNotFoundException::new);
+    }
+}
