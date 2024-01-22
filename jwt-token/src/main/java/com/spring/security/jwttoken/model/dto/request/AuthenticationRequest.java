5f491c33b5e5640c04f6e5862175b405e6dba2b7
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-01-22 11:10:31 +0100

    Create AuthenticationRequest.java

---
+++ jwt-token/src/main/java/com/spring/security/jwttoken/model/dto/request/AuthenticationRequest.java
@@ -0,0 +1,19 @@
+package com.spring.security.jwttoken.model.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+
+    private String email;
+    private String password;
+
+}
