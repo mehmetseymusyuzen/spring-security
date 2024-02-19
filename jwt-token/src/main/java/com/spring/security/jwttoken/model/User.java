ca9230407c5ee1bb2df4c14ed8f3b0bf5e1d8642
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-02-19 20:12:12 +0100

    refactor User.java --> refactor Override method getAuthorities

--- jwt-token/src/main/java/com/spring/security/jwttoken/model/User.java
+++ jwt-token/src/main/java/com/spring/security/jwttoken/model/User.java
@@ -16,7 +16,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -56,7 +55,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        return role.getAuthorities();
     }
 
     @Override
