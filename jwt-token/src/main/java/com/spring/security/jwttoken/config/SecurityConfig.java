ae1fd8ffdeb995e199f10c103ab4f0665258747d
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-02-20 22:14:20 +0100

    refactor SecurityConfig.java --> added role configuration

--- jwt-token/src/main/java/com/spring/security/jwttoken/config/SecurityConfig.java
+++ jwt-token/src/main/java/com/spring/security/jwttoken/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,8 +15,24 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
+import static com.spring.security.jwttoken.model.Permission.ADMIN_CREATE;
+import static com.spring.security.jwttoken.model.Permission.ADMIN_DELETE;
+import static com.spring.security.jwttoken.model.Permission.ADMIN_READ;
+import static com.spring.security.jwttoken.model.Permission.ADMIN_UPDATE;
+import static com.spring.security.jwttoken.model.Permission.MANAGER_CREATE;
+import static com.spring.security.jwttoken.model.Permission.MANAGER_DELETE;
+import static com.spring.security.jwttoken.model.Permission.MANAGER_READ;
+import static com.spring.security.jwttoken.model.Permission.MANAGER_UPDATE;
+import static com.spring.security.jwttoken.model.Role.ADMIN;
+import static com.spring.security.jwttoken.model.Role.MANAGER;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
@@ -32,6 +49,19 @@ public class SecurityConfig {
                 .authorizeHttpRequests(httpRequest ->
                         httpRequest
                                 .requestMatchers("/api/v1/auth/**").permitAll()
+
+                                .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(), MANAGER.name())
+                                .requestMatchers(GET, "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
+                                .requestMatchers(POST, "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(), MANAGER_CREATE.name())
+                                .requestMatchers(PUT, "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(), MANAGER_UPDATE.name())
+                                .requestMatchers(DELETE, "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(), MANAGER_DELETE.name())
+
+/*                                .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
+                                .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
+                                .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
+                                .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
+                                .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())*/
+
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(httpRequest ->
