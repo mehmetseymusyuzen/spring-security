887ad754cd71d13754f3880c2697e57ff055f0d3
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-03-20 22:20:31 +0100

    refactor JwtTokenApplication.java

--- jwt-token/src/main/java/com/spring/security/jwttoken/JwtTokenApplication.java
+++ jwt-token/src/main/java/com/spring/security/jwttoken/JwtTokenApplication.java
@@ -1,13 +1,58 @@
 package com.spring.security.jwttoken;
 
+import com.spring.security.jwttoken.model.dto.request.AuthenticationRequest;
+import com.spring.security.jwttoken.model.dto.request.RegisterRequest;
+import com.spring.security.jwttoken.service.AuthenticationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.spring.security.jwttoken.model.Role.ADMIN;
 
 @SpringBootApplication
 public class JwtTokenApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JwtTokenApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JwtTokenApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner runner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var adminRegisterRequest = RegisterRequest.builder()
+                    .firstname("admin")
+                    .lastname("admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            service.register(adminRegisterRequest);
+
+            var adminAuthRequest = AuthenticationRequest.builder()
+                    .email(adminRegisterRequest.getEmail())
+                    .password(adminRegisterRequest.getPassword())
+                    .build();
+
+            System.out.println("admin token : " + service.authenticate(adminAuthRequest).getAccessToken());
+
+/*            var managerRegisterRequest = RegisterRequest.builder()
+                    .firstname("manager")
+                    .lastname("manager")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            service.register(managerRegisterRequest);
+
+            var managerAuthRequest = AuthenticationRequest.builder()
+                    .email(managerRegisterRequest.getEmail())
+                    .password(managerRegisterRequest.getPassword())
+                    .build();
+
+            System.out.println("manager token : " + service.authenticate(managerAuthRequest).getAccessToken());*/
+        };
+    }
 }
